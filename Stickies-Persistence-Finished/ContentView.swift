825173af8e5574cf45532f99a9ca12253dfb41ee
justinwongeecs <jwonghelloworld@berkeley.edu>
2025-04-03 13:57:11 -0700

    Initial Commit

---
+++ Stickies-Persistence-Finished/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stickies-Persistence-Finished
+//
+//  Created by Justin Wong on 4/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stickies-Persistence-Finished/Stickies_Persistence_FinishedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stickies_Persistence_FinishedApp.swift
+//  Stickies-Persistence-Finished
+//
+//  Created by Justin Wong on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Stickies_Persistence_FinishedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
